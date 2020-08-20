e4bce0c7b43731409a9be4104bde382faeb82d17
Kayla Folwick <57514529+kfolwick@users.noreply.github.com>
2020-08-19 18:57:37 -0700

    Initial Component setup
    Co-Authored-By: Max Catozzi <mxcatozzi@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import Blog from "./Components/Blog";
+import BlogPost from "./Components/BlogPost";
+import Contact from "./Components/Contact";
+import EditBlogPost from "./Components/EditBlogPost";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import NewBlogPost from "./Components/NewBlogPost";
+
 function App() {
   return (
     <Router>
