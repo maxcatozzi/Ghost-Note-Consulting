03e52e028c7396518b13a07d6bbb74649d56ecca
Kayla Folwick <57514529+kfolwick@users.noreply.github.com>
2020-08-27 20:32:14 -0700

    added video background
    Co-Authored-By: Max Catozzi <mxcatozzi@gmail.com>

--- src/Components/Hamburger.js
+++ src/Components/Hamburger.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Hamburger.css'
 
 function Hamburger() {
   return (
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import video from '../video/record_vid.mp4'; 
+import './Home.css';
+import { useMediaQuery } from 'react-responsive';
+
 
 function Home() {
+
+  // const largeView = useMediaQuery({
+  //   query: '(min-width: 800px)'
+  // })
+
+  // const extraLargeView = useMediaQuery({
+  //   query: '(min-width: 800px)'
+  // })
+
+
   return (
     <div>
-      <h1>Home!</h1>
-      <h2 style={{color: "blue"}}>Hiii</h2>
+      <div>
+        <div className='videoContainer'>
+          <video className='recordVid'autoPlay loop muted>
+            <source src={video} type='video/mp4' />
+          </video>
+          <div className='foregroundLayer'>
+          </div>
+          <div className='greeting'>
+            <h1>Cool Slogan</h1>
+            <h2>Another cool slogan</h2>
+          </div>
+        </div>
+      </div>
+      {/* <div className='videoOverlay'></div>
+      <div>Hi</div> */}
     </div>
   );
 }
