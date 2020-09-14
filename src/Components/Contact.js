b3854c877e2fccffd630ad1c17d619b7b0a79d2c
Kayla Folwick <57514529+kfolwick@users.noreply.github.com>
2020-09-14 14:09:51 -0700

    Adds Google reCaptcha to contact form
    Co-Authored-By: Max Catozzi <mxcatozzi@gmail.com>

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './Contact.css';
 import axios from 'axios';
+import ReCAPTCHA from "react-google-recaptcha";
 
 class Contact extends Component {
   constructor() {
     super();
     this.state = {
-        name: '',
-        email: '',
-        phoneNumber: ''
+      name: '',
+      email: '',
+      phoneNumber: '',
+      isVerified: false
     }
   }
 
@@ -21,23 +23,41 @@ class Contact extends Component {
         console.log('name:', this.state.name);
         console.log('email:', this.state.email);
         console.log('phone number:', this.state.phoneNumber);
-    });
+    }
+    );
+  }
+
+  recaptchaLoaded = () => {
+    console.log('successfully loaded');
+  }
+
+  verifyRecaptcha = (response) => {
+    console.log(response);
+    if (response) {
+      this.setState({
+        isVerified: true
+      });
+    }
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
+    if (this.state.isVerified) {
     const message = {message: `This person would like you to get in contact with them! 
-      Name: ${this.state.name} 
-      Email: ${this.state.email} 
-      Phone Number: ${this.state.phoneNumber}`};
-    axios.post('http://localhost:5000/emails/send', message)
-      .then(res => {
-        alert('Your message was sent!');
-        console.log('success');
-      })
-      .catch(err => {
-        console.log(err);
-      })
+        Name: ${this.state.name} 
+        Email: ${this.state.email} 
+        Phone Number: ${this.state.phoneNumber}`};
+      axios.post('http://localhost:5000/emails/send', message)
+        .then(res => {
+          alert('Your message was sent!');
+          console.log('success');
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    } else {
+      alert('Are you a robot?')
+    }
   }
 
   render() {
@@ -50,8 +70,12 @@ class Contact extends Component {
           <div><input name='phoneNumber' value={this.state.phoneNumber} type='text' placeholder='Your Phone Number' onChange={this.handleChange}></input></div>
           <div><button type='submit'>Submit</button></div>
         </form>
+        <ReCAPTCHA
+          sitekey='6LcBOswZAAAAABoklOrxUh4u6eF0f52vKue87Fys'
+          onChange={this.verifyRecaptcha}
+          theme='dark'
+        />
       </div>
-
     );
   }
 }
