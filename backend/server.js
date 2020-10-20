88b0c3ef2c63a7a4f3d1b3ec731cbe416e2c4b6e
Kayla Folwick <57514529+kfolwick@users.noreply.github.com>
2020-10-20 16:16:47 -0700

    temp commit

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,34 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
+// const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// const cookieParser = require('cookie-parser');
 
-require('dotenv').config();
+// require('dotenv').config();
 
-const app = express();
-const port = process.env.PORT || 5000;
+// const app = express();
+// const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
+// app.use(cors());
+// app.use(express.json());
+// app.use(cookieParser());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
-})
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//   console.log('MongoDB database connection established successfully');
+// })
 
-const blogPostsRouter = require('./routes/blogPosts');
-const usersRouter = require('./routes/users');
-const emailsRouter = require('./routes/emails');
+// const blogPostsRouter = require('./routes/blogPosts');
+// const usersRouter = require('./routes/users');
+// const emailsRouter = require('./routes/emails');
 
-app.use('/blogPosts', blogPostsRouter);
-app.use('/users', usersRouter);
-app.use('/emails', emailsRouter);
+// app.use('/blogPosts', blogPostsRouter);
+// app.use('/users', usersRouter);
+// app.use('/emails', emailsRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on port: ${port}`);
+// });
 
 
