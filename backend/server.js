59ca3eac19c975400abb8974e6af3fdd50a9bf7d
Kayla Folwick <57514529+kfolwick@users.noreply.github.com>
2020-10-21 16:08:06 -0700

    Takes out recaptcha and adds current build folder
    Co-Authored-By: Max Catozzi <mxcatozzi@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,40 @@
-// const express = require('express');
-// const cors = require('cors');
-// const mongoose = require('mongoose');
-// const cookieParser = require('cookie-parser');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const path = require('path');
 
-// require('dotenv').config();
+require('dotenv').config();
 
-// const app = express();
-// const port = process.env.PORT || 5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
-// app.use(cors());
-// app.use(express.json());
-// app.use(cookieParser());
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../build')));
 
-// const uri = process.env.ATLAS_URI;
-// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
-// const connection = mongoose.connection;
-// connection.once('open', () => {
-//   console.log('MongoDB database connection established successfully');
-// })
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+})
 
-// const blogPostsRouter = require('./routes/blogPosts');
-// const usersRouter = require('./routes/users');
-// const emailsRouter = require('./routes/emails');
+const blogPostsRouter = require('./routes/blogPosts');
+const usersRouter = require('./routes/users');
+const emailsRouter = require('./routes/emails');
 
-// app.use('/blogPosts', blogPostsRouter);
-// app.use('/users', usersRouter);
-// app.use('/emails', emailsRouter);
+app.use('/blogPosts', blogPostsRouter);
+app.use('/users', usersRouter);
+app.use('/emails', emailsRouter);
 
-// app.listen(port, () => {
-//   console.log(`Server is running on port: ${port}`);
-// });
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
 
 
